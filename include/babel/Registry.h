909c2e741ac9e0ce8840f8b176d07e3d03daf9cd
David Schalig <github@schalig.org>
2011-12-25 19:34:01 +0900

    add object registry class

---
+++ include/babel/Registry.h
@@ -0,0 +1,77 @@
+#ifndef bbRegistry_H_
+#define bbRegistry_H_
+
+#include "Arr.h"
+
+/** Registry object container, helper for bbRegistry. */
+struct bbRegistryObject
+{
+    void* pObj;
+};
+
+#if bbSIZEOF_UPTR == 8
+bbDECLAREARR(bbRegistryObject, bbArrRegistryObject, 8);
+#else
+bbDECLAREARR(bbRegistryObject, bbArrRegistryObject, 4);
+#endif
+
+/** Object registry. */
+struct bbRegistry
+{
+    bbArrRegistryObject mEntries;
+
+    ~bbRegistry()
+    {
+        bbASSERT(GetCount() == 0);
+    }
+
+    inline bbUINT GetSize() const { return mEntries.GetSize(); }
+
+    inline void* GetObj(int const id)
+    {
+        return mEntries[id].pObj;
+    }
+
+    int NextID();
+
+    /** Add an object to the registry.
+        Double pointers are not checked.
+        @param pObj Object pointer
+        @return Unique registry ID, or -1 on error
+    */
+    int Add(void* const pObj);
+
+    /** Delete an object from the registry by its registry ID.
+        If the passed ID is -1, the call has no effect.
+        @param id Object ID as returned by Add(), can be -1
+    */
+    inline void Delete(int const id)
+    {
+        bbASSERT(id < (int)mEntries.GetSize());
+        if (id >= 0)
+            mEntries[id].pObj = NULL;
+    }
+
+    /** Delete an object from the registry.
+        Only first hit will be deleted. No effect if object not found.
+        @param pObj Object to remove.
+    */
+    inline void DeleteObj(void* const pObj)
+    {
+        Delete(Find(pObj));
+    }
+
+    /** Find object in registry.
+        @param pObj Object to search for
+        @return ID if found, or -1 if not found.
+    */
+    int Find(void* const pObj);
+
+    /** Get number of objects in registry.
+        @return Object count
+    */
+    bbUINT GetCount() const;
+};
+
+#endif
+
---
+++ src/Registry.cpp
@@ -0,0 +1,54 @@
+#include "Registry.h"
+
+int bbRegistry::NextID()
+{
+    int id;
+    for (id=0; id<(int)mEntries.GetSize(); id++)
+    {
+        if (mEntries[id].pObj == NULL)
+            break;
+    }
+    return id;
+}
+
+int bbRegistry::Add(void* const pObj)
+{
+    int id = NextID();
+
+    if (id == mEntries.GetSize())
+    {
+        if (!mEntries.Grow(1))
+            return -1;
+    }
+
+    mEntries[id].pObj = pObj;
+    return id;
+}
+
+bbUINT bbRegistry::GetCount() const
+{
+    bbUINT count = 0;
+    const bbRegistryObject* p = mEntries.GetPtr();
+    const bbRegistryObject* const p_end = mEntries.GetPtrEnd();
+
+    while (p < p_end)
+    {
+        if (p->pObj)
+            count++;
+        p++;
+    }
+
+    return count;
+}
+
+int bbRegistry::Find(void* const pObj)
+{
+    int i = mEntries.GetSize();
+    for(;;)
+    {
+        if (--i < 0) break;
+        if (mEntries[i].pObj == pObj) break;
+    }
+    return i;
+}
+
