9b10b301a3ccaed897ec072b603dd148c7fa0492
David Schalig <github@schalig.org>
2012-05-09 17:05:37 +0900

    fix address calculation in bbST64LE macro

--- include/babel/defs.h
+++ include/babel/defs.h
@@ -555,7 +555,7 @@ typedef struct bbENCSTATE bbENCSTATE;
 /** Store bbU32 (32 bit) to unaligned address, stores in little endian order */
 #define bbST32LE(adr,w) *(bbU8*)(adr)=(bbU8)(w),*((bbU8*)(adr)+1)=(bbU8)((w)>>8),*((bbU8*)(adr)+2)=(bbU8)((w)>>16),*((bbU8*)(adr)+3)=(bbU8)((w)>>24)
 /** Store bbU64 (64 bit) to unaligned address, stores in little endian order */
-#define bbST64LE(adr,w) bbST32LE(adr,(bbU32)w),bbST32LE(adr,(bbU32)(w>>32))
+#define bbST64LE(adr,w) bbST32LE(adr,(bbU32)w),bbST32LE((bbU8*)(adr)+4,(bbU32)(w>>32))
 /** Load little endian bbS16 (16 bit) from unaligned address and sign-extend */
 #define bbLDS16LE(adr) (bbS32)((bbU32)*((bbU8*)(adr))|((bbS32)*(((bbS8*)(adr))+1)<<8))
 /** Load little endian 24 bit word (low 24 bit of a bbU32) from unaligned address and sign-extend */
